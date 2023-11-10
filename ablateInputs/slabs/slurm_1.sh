89adc4e6a8107ec9c075dc1b0ff4afbf7f6e6bee
Alex Becerra <70654720+ajbecerr@users.noreply.github.com>
2023-11-10 12:25:33 -0500

    Update slurm_1.sh

--- ablateInputs/slabs/slurm_1.sh
+++ ablateInputs/slabs/slurm_1.sh
@@ -7,11 +7,11 @@
 #SBATCH -A sunyb
 #SBATCH --mail-user=ajbecerr@buffalo.edu
 # module load python
-# cd ../../preprocessing/
-# python perturbMechs.py $1 $2
-# python perturbSlabs.py $1 $2
-# cd ../ablateInputs/slabs/
-# module purge
+cd ../../preprocessing/
+python perturbMechs.py $1 $2
+python perturbSlabs.py $1 $2
+cd ../ablateInputs/slabs/
+module purge
 module load gcc/10.3.1-magic 
 module load cmake/3.25.2
 export PETSC_DIR="/usr/workspace/ajbecerr/petsc" #UPDATE to the real path of petsc
@@ -19,5 +19,5 @@ export PETSC_ARCH="arch-ablate-opt-gcc"
 export PKG_CONFIG_PATH="${PETSC_DIR}/${PETSC_ARCH}/lib/pkgconfig:$PKG_CONFIG_PATH"
 export HDF5_ROOT="${PETSC_DIR}/${PETSC_ARCH}"  
 export PATH="${PETSC_DIR}/${PETSC_ARCH}/bin:$PATH"
-srun -n36 /usr/workspace/ajbecerr/ablateOpt/ablate --input /p/lustre2/ubchrest/CMAME_Final/ablateInputs/slabs/slabBurner.2D.pmma.1_soot_updated.yaml
-# srun -n36 /usr/workspace/ajbecerr/ablateOpt/ablate --input /p/lustre2/ubchrest/CMAME_Final/ablateInputs/slabs/slabBurner.2D.$1.$2.yaml
+# srun -n36 /usr/workspace/ajbecerr/ablateOpt/ablate --input /p/lustre2/ubchrest/CMAME_Final/ablateInputs/slabs/slabBurner.2D.pmma.1_soot_updated.yaml
+srun -n36 /usr/workspace/ajbecerr/ablateOpt/ablate --input /p/lustre2/ubchrest/CMAME_Final/ablateInputs/slabs/slabBurner.2D.$1.$2.yaml
