002c73aa7e4b8c00a761e190011c8a2fff4d3c12
Alejandro Becerra <ajbecerr@quartz188.llnl.gov>
2023-12-04 15:16:28 -0800

    inital post

--- ablateOutputs/getQoIs.py
+++ ablateOutputs/getQoIs.py
@@ -1,22 +1,22 @@
 import glob
 import numpy as np
 
-domains = glob.glob('domain*.npy')
+# domains = glob.glob('domain*.npy')
 
-for domain in domains:
-    domain_i = np.load(domain, allow_pickle=True)
-    i = int(domain.split('_')[1].split('.')[0])
+# for domain in domains:
+    # domain_i = np.load(domain, allow_pickle=True)
+    # i = int(domain.split('_')[1].split('.')[0])
 
-    QoIs = []
-    for j in range(3216):
-        mask_j_k = np.array([k % 3216 == j for k in range(len(domain_i))])
-        domain_i_j_k = domain_i[mask_j_k, :]
-        Tmean = sum(domain_i_j_k[:, 1])/len(domain_i_j_k[:, 1])
-        Vmean = sum(domain_i_j_k[:, 2])/len(domain_i_j_k[:, 2])
-        QoIs.append(np.array([Tmean, Vmean, domain_i[j, -2], domain_i[j, -1]]))
+    # QoIs = []
+    # for j in range(3216):
+        # mask_j_k = np.array([k % 3216 == j for k in range(len(domain_i))])
+        # domain_i_j_k = domain_i[mask_j_k, :]
+        # Tmean = sum(domain_i_j_k[:, 1])/len(domain_i_j_k[:, 1])
+        # Vmean = sum(domain_i_j_k[:, 2])/len(domain_i_j_k[:, 2])
+        # QoIs.append(np.array([Tmean, Vmean, domain_i[j, -2], domain_i[j, -1]]))
 
-    print(np.array(QoIs))
-    np.save(domain.split('_')[0]+'_'+str(i)+'_B.npy', np.array(QoIs))
+    # print(np.array(QoIs))
+    # np.save(domain.split('_')[0]+'_'+str(i)+'_B.npy', np.array(QoIs))
 
 boundaries = glob.glob('boundary*.npy')
 
