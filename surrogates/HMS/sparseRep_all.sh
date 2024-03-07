23176ed18001a23cc6683a4315a03da36adda5df
Alex Becerra <abecer02@login-prod-02.pax.tufts.edu>
2024-03-07 15:26:43 -0500

    Tufts changes

--- surrogates/HMS/sparseRep_all.sh
+++ surrogates/HMS/sparseRep_all.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
-sbatch sparseRep_1.sh 12 0.005 rdot MMA location
-sbatch sparseRep_1.sh 12 0.005 rdot MMA sample
+# sbatch sparseRep_1.sh 12 0.005 rdot MMA location
+# sbatch sparseRep_1.sh 12 0.005 rdot MMA sample
 
-sbatch sparseRep_1.sh 12 0.005 qtot MMA location
-sbatch sparseRep_1.sh 12 0.005 qtot MMA sample
+# sbatch sparseRep_1.sh 12 0.005 qtot MMA location
+# sbatch sparseRep_1.sh 12 0.005 qtot MMA sample
+
+sbatch sparseRep_1.sh 6 0.00005 rdot MMA location
+sbatch sparseRep_1.sh 6 0.00005 rdot MMA sample
+
+sbatch sparseRep_1.sh 6 0.00005 qtot MMA location
+sbatch sparseRep_1.sh 6 0.00005 qtot MMA sample
