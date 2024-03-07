612daa7676d2748259cb44856f0eec0ad3162c70
Alejandro Becerra <ajbecerr@srv-p22-13.cbls.ccr.buffalo.edu>
2024-03-07 08:53:08 -0500

    CCR updates

--- ablateInputs/0dIgnition/slurm.sh
+++ ablateInputs/0dIgnition/slurm.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 #SBATCH --partition=general-compute
 #SBATCH --qos=general-compute
+#SBATCH --constraint=LEGACY
 #SBATCH --job-name='chem_sens'
 #SBATCH --output=out_chem_sens-%j.out
 #SBATCH --error=error_chem_sens-%j.err
@@ -10,6 +11,7 @@
 #SBATCH --mail-user=ajbecerr@buffalo.edu
 #SBATCH --mail-type=ALL
 module purge
+module load ccrsoft/legacy
 module load dakota/6.15
 export DAK_INSTALL=/util/academic/dakota/dakota-6.15.0-release-public-rhel7.x86_64-gui_cli
 export PATH=$DAK_INSTALL/bin:$DAK_INSTALL/share/dakota/test:$PATH
